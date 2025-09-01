5f26f4b32b3044873d28013ef4d0d7616dad01c3
muhammadsamimreza <muhammadsamimreza@gmail.com>
2025-09-02 01:09:32 +0600

    Delete Button Functionality

--- js/script.js
+++ js/script.js
@@ -1,10 +1,11 @@
 const addButton = document.getElementById("addBtn");
-
+// Add list Functionality Here.........///
 const addList = () => {
   const inputField = document.getElementById("inputField");
   const textValue = inputField.value;
   
-  const ul = document.getElementById("ulPage");
+  if(textValue !== ''){
+    const ul = document.getElementById("ulPage");
   const li = document.createElement("li");
   li.innerHTML = `
     <input type="checkbox">
@@ -13,8 +14,22 @@ const addList = () => {
    `;
 
    ul.appendChild(li);
+   inputField.value ='';
+  }else{
+    alert("Enter a valid text")
+  }
+  
 };
 
 addButton.addEventListener("click", () => {
   addList();
 });
+
+
+//  Delete Button Functionality Here..........//
+
+deleteBtn.addEventListener("click", ()=>{
+  const ul =  document.getElementById('ulPage');
+  
+  
+})
